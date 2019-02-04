fa164e280b70d14b97d544338243a3ceda1e2cef
Karim ElGhaazouly <karimelghazouly@gmail.com>
2019-02-05 00:09:09 +0200

    more organized project structure

---
+++ controllers/todo_controller.js
@@ -0,0 +1,14 @@
+module.exports = function(app)
+{
+    app.get('/todo',function(req,resp){
+
+    });
+
+    app.post('/todo',function(req,resp){
+
+    });
+
+    app.delete('/todo',function(req,resp){
+
+    });
+};
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let express = require('express');
 let app = express();
-
+let todoController = require('./controllers/todo_controller');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+todoController(app);
 
 app.listen(3000);
 console.log("http://127.0.0.1:3000");
