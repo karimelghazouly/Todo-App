45aa861e3399eb99fdfe695fcf9edc2b09ac9455
karimelghazouly <karimelghazouly@gmail.com>
2019-02-27 23:11:08 +0200

    configuration for heroku deployment

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 let express = require('express');
 let app = express();
 let todoController = require('./controllers/todo_controller');
-
+let authConroller = require('./controllers/auth_controller');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.get('/',function(req,res){
+    res.render('home');
+})
+
+authConroller(app);
 todoController(app);
 
-app.listen(3000);
-console.log("http://127.0.0.1:3000");
+app.listen(process.env.PORT || 5000);
