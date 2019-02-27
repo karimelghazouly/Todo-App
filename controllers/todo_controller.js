cbd41e02c2a18fcb65680f6f1f1899d8ac1a5f7c
karimelghazouly <karimelghazouly@gmail.com>
2019-02-28 00:40:21 +0200

    fix error in controller

--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -1,53 +1,29 @@
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var userdata = require('../userdata');
-
+var todomodel = require('../models/todo_model');
 var urlparser = bodyParser.urlencoded({extended : false});
-mongoose.connect('mongodb://admin:admin123@ds127995.mlab.com:27995/todos', { useNewUrlParser: true });
 
-var todoSchema = new mongoose.Schema({
-    userid: String,
-    item: String
-});
-
-var Todo = mongoose.model('Todo', todoSchema);
 
 module.exports = function(app)
 {
     app.get('/todo',function(req,resp){
-        
-        Todo.find({userid:userdata.userid}, function(err, data){
-            if(err)
-                throw err;
-            resp.render('todo', {todos:data});
-        });
-        
+        todomodel.FindTodo(resp);
     });
 
     app.post('/todo', urlparser, function(req,resp){
         ndata = req.body;
         ndata.userid = userdata.userid;
         
-        newItem = Todo(ndata).save(function(err, data){
-            if(err)
-                throw err;
-            
-            resp.json(data);
-        });
-        
+        todomodel.SaveTodo(resp, ndata);
     });
 
     app.delete('/todo/:item', function(req,resp){
         
         name = req.params.item.substr(1);
+        name = name.replace(/-/g, " ");
         id = userdata.userid;
-        Todo.find({userid: id, item: name }).deleteOne(function(err, data){
-            if(err)
-                throw err;
-
+        data = {userid: id, item: name };
             
-            resp.json(data);
-        })
-        
+        todomodel.DeleteTodo(resp, data);       
     });
 };
